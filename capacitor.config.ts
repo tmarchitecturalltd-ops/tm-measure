865908b3b62675885c2bff056cb4a9cc81220ee0
Harry <tmarchitecturalltd@gmail.com>
2026-06-14 13:56:10 +0100

    Align package name to ltd.tmdesigns.tmmeasure

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.tmdesigns.portal",
-  appName: "TM Designs Portal",
+  appId: "ltd.tmdesigns.tmmeasure",
+  appName: "TM Measure",
   webDir: "out",
   server: {
     androidScheme: "https",
