b5822fd4030ec3423470254b587cab7c94f963f8
Harry <tmarchitecturalltd@gmail.com>
2026-07-01 13:30:38 +0100

    Fix WebView debugging + bump versionCode to 1026

---
+++ android/app/src/main/java/ltd/tmdesigns/tmmeasure/MainActivity.java
@@ -0,0 +1,13 @@
+package ltd.tmdesigns.tmmeasure;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView.setWebContentsDebuggingEnabled(true);
+    }
+}
