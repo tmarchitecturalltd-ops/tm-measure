3c4387d926cc7992c0ea72e59cc54c464eab8cab
tmarchitecturalltd-ops <tmarchitecturalltd@gmail.com>
2026-07-02 19:00:39 +0100

    fix: add BuildConfig import for WebView debug flag

--- android/app/src/main/java/ltd/tmdesigns/tmmeasure/MainActivity.java
+++ android/app/src/main/java/ltd/tmdesigns/tmmeasure/MainActivity.java
@@ -3,6 +3,7 @@ package ltd.tmdesigns.tmmeasure;
 import android.os.Bundle;
 import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
+import ltd.tmdesigns.tmmeasure.BuildConfig;
 
 public class MainActivity extends BridgeActivity {
     @Override
